71353bed33994ae256feeb9015833c9542395c7a
Nikita-Gilev <87444977+Nikita-Gilev@users.noreply.github.com>
2021-07-28 14:11:09 +0300

    Create HW 3-2.py

---
+++ HW 3-2.py
@@ -0,0 +1,4 @@
+  
+def my_func(name, surname, byear, city, email, phone):
+    print(name, surname, byear, city, email, phone)
+print(my_func(surname = 'Гилёв', name = 'Никита', byear = '1988', city = 'Санкт-Петербург', email = 'example@gmail.com', phone = '8-999-999-99-99'))
